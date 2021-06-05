cffe9064d2e70648469effdb0eb372026a053ffb
AnasBInRiaz123 <79161599+AnasBInRiaz123@users.noreply.github.com>
2021-06-05 22:52:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+var list = document.getElementById('list');
+
+function addTodo() {
+    var to_do = document.getElementById('todo');
+    if (to_do.value.length == 0)
+      { 
+         alert("Please Enter any Value");  	
+         return false; 
+      }  	
+    
+
+
+    var li = document.createElement('li');
+    var liText = document.createTextNode(to_do.value);
+    li.appendChild(liText)
+    
+
+
+    var delBtn = document.createElement("img");
+    delBtn.setAttribute("src","unnamed.png")
+    // var delText = document.createTextNode("Delete");
+    delBtn.setAttribute("class", "btn1");
+    delBtn.setAttribute("onclick", "delItem(this)");
+    // delBtn.appendChild(delText)
+
+    var editBtn = document.createElement("img");
+    editBtn.setAttribute("src","unnamed (1).png")
+    // var editText = document.createTextNode("EDIT");
+    editBtn.setAttribute("class", "btn2");
+    editBtn.setAttribute("onclick", "editItem(this)");
+    // editBtn.appendChild(editText)
+
+
+
+    li.appendChild(delBtn);
+    li.appendChild(editBtn)
+
+    list.appendChild(li);
+    to_do.value = ""
+
+}
+
+ 
+function delItem(e) {
+    e.parentNode.remove()
+}
+
+
+function editItem(e){
+    var val=e.parentNode.firstChild
+    var edit=prompt("enter Edited values",val)
+    val.nodeValue=edit
+}
+
+
+function deleteAll() {
+    list.innerHTML = "";
+}
